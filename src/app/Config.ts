1fc7bfe7876cb26678a6ccf4929ba5b00ef40028
usmankhanbrainplow <usman.khanbrainplow.com>
2018-06-09 11:10:24 +0500

    sdfs

--- src/app/Config.ts
+++ src/app/Config.ts
@@ -2,17 +2,9 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class Config {//https://apis.choicegenie.com/choice/
-<<<<<<< HEAD
-    public static api: String = 'https://apis.choicegenie.com/';
-=======
-<<<<<<< HEAD
-   // public static api: String = 'https://apis.choicegenie.com/choice/';
+
  public static api: String = 'https://apis.choicegenie.com/';
-=======
-    public static api: String = 'https://apis.choicegenie.com/choice/';
->>>>>>> 3546fd40cac579094e2c76469db2331d09c8b58e
-//    public static api: String = 'https://apis.choicegenie.com/';
->>>>>>> 5e8bb451289be923c1a8c45687ffb32dc03e36b2
+
 //    public static api: String = 'http://192.168.30.237:9000/choice/';
    public static Imageurl: string = 'https://storage.choicegenie.com/media/'; 
     public  static  Imageurlget = 'https://storage.choicegenie.com/images/';  
