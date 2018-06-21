77240a9b7385d2ab89e06e082fef4d3ec6cc0684
usmankhanbrainplow <usman.khanbrainplow.com>
2018-06-21 17:24:27 +0500

    qwqhjqwhqe

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import {FormControl, Validators} from '@angular/forms';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 // import {ContactUsService} from "./contact-us.service";
 import { AgmCoreModule } from '@agm/core';
 import { Headers, Http, Response } from '@angular/http';
@@ -12,17 +12,10 @@ import { SimpleGlobal } from 'ng2-simple-global';
 import { ResponseContentType } from '@angular/http/src/enums';
 import { FormBuilder, Validators, NgControl, RadioControlValueAccessor, FormControl, FormGroup } from '@angular/forms';
 import { HttpClient, HttpResponse, HttpHeaders } from "@angular/common/http";
- import swal from 'sweetalert2'; 
+import swal from 'sweetalert2';
 import { MatSelect } from '@angular/material';
 import { PasswordValidation } from './password-validator.component';
 
-//import { FormControl, FormGroup } from '@angular/forms';
-//import { signupdataService } from '../signup1/signupdata.service';
-//import { signupuserdata } from "./signup1data.service";
-// import { modelGroupProvider } from '@angular/forms/src/directives/ng_model_group';
-// import { TOUCHEND_HIDE_DELAY } from '@angular/material';
-
-//import {signupuserdata} from './signup1data.service';
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
@@ -76,27 +69,28 @@ export class ContactusComponent implements OnInit {
 
   //constructor() { }
 
+
   ngOnInit() {
     this.signupForm = this.fb.group({
-     //['', Validators.compose([Validators.required])],
+      //['', Validators.compose([Validators.required])],
       'name': ['', Validators.compose([Validators.required])],
       'mobno': ['', Validators.compose([Validators.required])],
       'msg': ['', Validators.compose([Validators.required])],
       'email': ['', Validators.compose([Validators.required])],
-      
+
       // 'Phone': ['', Validators.compose([Validators.required, Validators.pattern(this.digitsOnly)])],
       // 'country': ['', Validators.compose([Validators.required, Validators.pattern(this.normalPattern)])],
-      
+
     });
   }
-  
+
   onChange(e) {
     alert(e)
   }
   check(e) {
     console.log(this.model)
   }
-  
+
   Contactuserdata() {
     //alert('hello');
     console.log("CHOICE GENIE", this.model);
@@ -106,16 +100,16 @@ export class ContactusComponent implements OnInit {
 
     headers.append('Content-Type', 'application/json');
     // this.http.get(Config.api + 'data_against_zipcode/' + this.zip_code + '', { headers: headers }),
-    this.http.post(Config.api + 'contactus/',this.model , { headers: headers })
+    this.http.post(Config.api + 'contactus/', this.model, { headers: headers })
 
 
-     .subscribe(Res => {
+      .subscribe(Res => {
         console.log(Res);
         // this.next = Res[0].next;
 
         console.log(this.model);
         swal({
-          text: "Thank you for Successflluy contact!",
+          text: "Thank you for Successflluy Contact Us !",
           title: "Choice Genie",
           type: "success",
           showConfirmButton: false,
@@ -124,12 +118,13 @@ export class ContactusComponent implements OnInit {
           confirmButtonText: "OK",
 
         })
-
+     //   this.name.clear();
+        // this.model.clear();
         //this.router.navigate(['/pages/login'])
       },
         error => {
           console.log(error);
-        //  this.toastr.error(error, null, {toastLife: 5000});
+          //  this.toastr.error(error, null, {toastLife: 5000});
           swal(
             'Invalid',
             'Please Try Again!',
@@ -137,7 +132,7 @@ export class ContactusComponent implements OnInit {
           )
 
         });
- 
+
 
 
   }
